afb90f0b2a0cf15e95691e761ae625ee32714e88
curegit <37978051+curegit@users.noreply.github.com>
2022-12-27 11:24:40 +0900

    Add TSs

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from "./views/HomeView.vue";
 import WorkspaceView from "./views/AboutView.vue";
+import HomeView from "./views/HomeView.vue";
 
 export default createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: "/:catchAll(.*)",
-            redirect: { name: "home" },
-        },
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/about",
-            name: "about",
-            component: WorkspaceView,
-        },
-    ],
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: WorkspaceView,
+    },
+    {
+      path: "/:path(.*)*",
+      name: "NotFound",
+      redirect: { name: "home" },
+    },
+  ],
+  sensitive: true,
+});
