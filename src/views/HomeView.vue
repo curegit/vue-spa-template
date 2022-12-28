b21c17e7b3b1fe44757602839f9d8fdd23fa30f8
curegit <37978051+curegit@users.noreply.github.com>
2022-12-28 16:52:30 +0900

    Add icons home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import HelloWorld from "../components/HelloWorld.vue";
+
+import vueIcon from "../assets/vue.svg";
+import viteIcon from "../assets/vite.svg";
+import tsIcon from "../assets/typescript.svg";
+
+const icons = {
+  Vue: vueIcon,
+  Vite: viteIcon,
+  TypeScript: tsIcon,
+};
 </script>
 
 <template>
@@ -7,4 +17,15 @@ import HelloWorld from "../components/HelloWorld.vue";
     <h2>Home</h2>
     <HelloWorld message="Vite + Vue + TS" />
   </main>
+  <aside>
+    <figure v-for="(val, key) in icons" :key="key">
+      <img class="icon" :src="val" :alt="key" />
+    </figure>
+  </aside>
 </template>
+
+<style scoped>
+.grid {
+  display: grid;
+}
+</style>
