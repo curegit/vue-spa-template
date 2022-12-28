d74cc51e08bafdf1e61c6986b5a59cc585d8b2d4
curegit <37978051+curegit@users.noreply.github.com>
2022-12-28 15:24:23 +0900

    Fix import order

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import AboutView from "./views/AboutView.vue";
 import HomeView from "./views/HomeView.vue";
+import AboutView from "./views/AboutView.vue";
 
 export default createRouter({
   history: createWebHistory(),
