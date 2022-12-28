2cb49977af0384cb2420cc91bf90d92b1a14dc90
curegit <37978051+curegit@users.noreply.github.com>
2022-12-28 15:17:17 +0900

    import SVG

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import ItemCard from "../components/ItemCard.vue";
+
+import vueIcon from "../assets/vue.svg";
+import viteSvg from "../assets/vite.svg";
+import tsIcon from "../assets/typescript.svg";
 </script>
 
 <template>
   <main>
     <h2>About</h2>
     <div class="cards">
-      <ItemCard heading="Vue" icon="/vue.svg">AAA</ItemCard>
-      <ItemCard heading="Vite" icon="/src/assets/vite.svg">AAA</ItemCard>
-      <ItemCard heading="TypeScript" icon="/src/assets/typescript.svg">AAA</ItemCard>
+      <ItemCard heading="Vue" :icon="vueIcon">
+        <p><strong>Vue</strong> is a front end JavaScript framework.</p>
+      </ItemCard>
+      <ItemCard heading="Vite" :icon="viteSvg">
+        <p>
+          <strong>Vite</strong>
+        </p>
+      </ItemCard>
+      <ItemCard heading="TypeScript" :icon="tsIcon">
+        <p>
+          <strong>TypeScript</strong>
+        </p>
+      </ItemCard>
     </div>
   </main>
 </template>
